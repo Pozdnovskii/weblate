2663a0aaa5c751b4256f94a5eeaac1f585ca1fcd
Ivan Pozdnovskii <ipozdnovskii@icloud.com>
2025-06-19 21:29:26 +0300

    Fix imports #6

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,6 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
-    resolve: {
-      alias: {
-        "@assets": "/src/assets",
-        "@components": "src/components",
-      },
-    },
   },
   site: "https://weblateweb.dev",
   integrations: [sitemap()],
