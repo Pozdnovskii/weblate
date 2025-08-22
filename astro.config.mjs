5eafa0ad3bac96eeaeaf7575c06ead240502586e
Ivan Pozdnovskii <ipozdnovskii@icloud.com>
2025-08-22 18:12:58 +0300

    Almost

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
@@ -10,4 +10,41 @@ export default defineConfig({
   },
   site: "https://weblateweb.dev",
   integrations: [sitemap()],
+  experimental: {
+    fonts: [
+      {
+        provider: "local",
+        name: "Space Grotesk",
+        cssVariable: "--font-space-grotesk",
+        variants: [
+          {
+            weight: 400,
+            style: "normal",
+            src: ["./src/assets/fonts/SpaceGrotesk-Regular.woff2"],
+          },
+          {
+            weight: 500,
+            style: "normal",
+            src: ["./src/assets/fonts/SpaceGrotesk-Medium.woff2"],
+          },
+        ],
+
+        fallbacks: ["sans-serif"],
+      },
+      {
+        provider: "local",
+        name: "Space Mono",
+        cssVariable: "--font-space-mono",
+        variants: [
+          {
+            weight: 400,
+            style: "normal",
+            src: ["./src/assets/fonts/SpaceMono-Regular.woff2"],
+          },
+        ],
+
+        fallbacks: ["monospace"],
+      },
+    ],
+  },
 });
